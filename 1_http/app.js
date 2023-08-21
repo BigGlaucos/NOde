b28442c63132440a1b2672bec75e1aee1177eaf7
BigGlaucos <glaucoscc@gmail.com>
2023-08-21 11:03:51 -0300

    changees

--- 1_http/app.js
+++ 1_http/app.js
@@ -1,27 +1,48 @@
-const express = require ("express");
-const {randomUUID} = require ("crypto");
+const express = require("express");
+const { randomUUID } = require("crypto");
+const { response } = require("express");
 const app = express();
 
-app.use(express.json() );
+app.use(express.json());
 
- const produtos = [];
-app.post("/produtos",(req,res)=>{
+const produtos = [];
+app.post("/produtos", (req, res) => {
+    const { name, price, description } = req.body;
 
- const {name, price, description} = req.body
- 
- const produto = {
-     id:randomUUID(),
-     name: name,
-     price: price ,
-     description : description
-    }
-    
+    const produto = {
+        id: randomUUID(),
+        name: name,
+        price: price,
+        description: description,
+    };
+
     produtos.push(produto);
 
-   return res.status(201).json(produtos);
+    return res.status(201).json(produtos);
+});
+
+app.get("/produtos", (req, res) => {
+    return res.json(produtos);
+}); 
+
+app.get("/produtos/:id",(req,res)=>{
+    const { id } = req.params
+
+    const produto = produtos.find((produto)=> produto.id === id);
+
+     return res.json(produto);
+});
+
+app.delete("/produtos/:id", (req, res) => {
+  const { id } = req.params;
+
+  const produto = produtos.findIndex((produto) => produto.id === id);
+
+  produtos.splice(produto,1);
+
+  return res.json(produto);
+   
 
 });
-app.listen(5000,() => console.log(`Servidor rodando na porta 5000`));
 
-
-
+app.listen(5000, () => console.log(`Servidor rodando na porta 5000`));
--- 1_http/index.js
+++ 1_http/index.js
@@ -11,51 +11,51 @@ const PORT = 3000
 //PRATICANDO
 //const server = http.createServer((req, res)=>{
 //    if(req.url === '/home'){
- //       res.writeHead(200, {'Content-Type':'Text/plan'} )
-  //      res.write('Página Home')
-   //     res.end()
+//       res.writeHead(200, {'Content-Type':'Text/plan'} )
+//      res.write('Página Home')
+//     res.end()
 
-  //  }else if(req.url === '/sobre'){
-  //      res.writeHead(200, {'Content-Type':'Text/plan'} )
-   //     res.write('Página Sobre')
-   //     res.end()
+//  }else if(req.url === '/sobre'){
+//      res.writeHead(200, {'Content-Type':'Text/plan'} )
+//     res.write('Página Sobre')
+//     res.end()
 
-   // }else{
-  //      res.writeHead(404, {'Content-Type':'Text/plan'} )
-   //     res.write('Página não encontrado')
-   //     res.end()
-   // }
+// }else{
+//      res.writeHead(404, {'Content-Type':'Text/plan'} )
+//     res.write('Página não encontrado')
+//     res.end()
+// }
 //})
 
 //Retornando HTML
-const server = http.createServer((req, res)=>{
-  if(req.url === '/home'){
-        res.writeHead(200, {'Content-Type':'Text/html'} )
-         res.write('<h1>Página Home<h1>')
-         res.write('<p>Esta é a Página Home<p>')
-         res.end()
-     
-   }else if(req.url === '/sobre'){
-        res.writeHead(200, {'Content-Type':'Text/html'} )
-        res.write('<h1>Página Sobre<h1>')
-        res.write('<p>Esta é a Página Sobre<p>')
-        res.end()
-        
-   }else if(req.url === '/Contato'){
-        res.writeHead(200, {'Content-Type':'Text/html'} )
-        res.write('<h1>Página Contato<h1>')
-        res.write('<p>Esta é a Página Contato<p>')
-        res.end()
-   
-   }else{
-        res.writeHead(404, {'Content-Type':'Text/html'} )
-        res.write('<h1>Página não encontrada!<h1>')
-        res.end()
+const server = http.createServer((req, res) => {
+     if (req.url === '/home') {
+          res.writeHead(200, { 'Content-Type': 'Text/html' })
+          res.write('<h1>Página Home<h1>');
+          res.write('<p>Esta é a Página Home<p>');
+          res.end();
 
-    }
-})
+     } else if (req.url === '/sobre') {
+          res.writeHead(200, { 'Content-Type': 'Text/html' })
+          res.write('<h1>Página Sobre<h1>');
+          res.write('<p>Esta é a Página Sobre<p>');
+          res.end();
+
+     } else if (req.url === '/Contato') {
+          res.writeHead(200, { 'Content-Type': 'Text/html' })
+          res.write('<h1>Página Contato<h1>');
+          res.write('<p>Esta é a Página Contato<p>');
+          res.end();
+
+     } else {
+          res.writeHead(404, { 'Content-Type': 'Text/html' })
+          res.write('<h1>Página não encontrada!<h1>')
+          res.end();
+
+     }
+});
 
 
-server.listen(PORT, ()=>{
-   console.log(`Servidor rondando na porta ${PORT}`)
-})
+server.listen(PORT, () => {
+     console.log(`Servidor rondando na porta ${PORT}`)
+});
