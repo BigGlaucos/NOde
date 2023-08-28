875ae90295f9a53b4cb331024a42439fcc4a38f6
BigGlaucos <glaucoscc@gmail.com>
2023-08-28 08:47:19 -0300

    Add

--- atvd/app.js
+++ atvd/app.js
@@ -25,6 +25,20 @@ app.post("/users", (req, res) => {
   return res.status(201).json(user);
 });
 
+app.put("/users/:id", (req,res)=>{
+    const {id} = req.params;
+    const {name,email,password} = req.body;
 
+    const user = users.find((u) => u.id ===id);
+
+    if (!user){
+        return res.status(404).json({error:"MISSING"})
+    }
+user.name = name;
+user.email = email;
+user.password = password;
+
+return res.status(200).json(user)
+});
 
 app.listen(PORT, () => console.log(`Servidor on-line porta: ${PORT}`));
