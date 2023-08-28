6d41910f51b5400eebd082259caf099f949ed1aa
BigGlaucos <glaucoscc@gmail.com>
2023-08-28 07:51:36 -0300

    Create

---
+++ atvd/app.js
@@ -0,0 +1,31 @@
+const express = require ("express");
+const { randomUUID} = require("crypto");
+const {response} = require("express");
+const {request} = require ("http");
+const app = express();
+
+PORT = 5000;
+
+app.use(express.json());
+
+
+const users = [];
+
+app.post("/users", (req,res)=>{
+     const {name,post,like} = req.body
+
+        const user = {
+            id:randomUUID,
+            name:name,
+            post:post,
+            like:like
+        }
+
+        users.push(user);
+
+        return res.status(201).json(users);
+})
+
+
+app.listen(PORT, ()=> CSSConditionRule.log(`Servidor on-line porta: ${PORT}`));
+
